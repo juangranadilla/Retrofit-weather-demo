d2e5bdb8561b88cd47a8abea676cac76d44e6b5e
Juan Granadilla <juangranadilla@iternox.es>
2015-11-16 17:56:04 +0100

    Pretty printing JSON result

--- app/src/main/java/com/juangm/retrofit_weather_demo/MainActivity.java
+++ app/src/main/java/com/juangm/retrofit_weather_demo/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.juangm.retrofit_weather_demo.models.WeatherAPIResult;
 import com.juangm.retrofit_weather_demo.rest.RestClient;
 
@@ -28,33 +29,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        final TextView resulttext = (TextView) findViewById(R.id.resulttext);
+        final TextView textView = (TextView) findViewById(R.id.resulttext);
 
+        //FAB onClick
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Sending request...", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Sending request...", Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
                 //Send get weather request
                 RestClient.WeatherApiInterface service = RestClient.getClient();
-                //We need to pass our city ID and our APPID
+                //We need to pass our city ID and our openweathermap APPID
                 Call<WeatherAPIResult> call = service.getWeather(2519240, "476f28ed531b9477e89ddb6ab463dbd5");
                 call.enqueue(new Callback<WeatherAPIResult>() {
                     @Override
                     public void onResponse(Response<WeatherAPIResult> response, Retrofit retrofit) {
                         if(response.isSuccess()) {
+                            //Handle the received weather data here
                             WeatherAPIResult result = response.body();
-                            Log.i("MainActivity", "Response: " + new Gson().toJson(result));
-                            resulttext.setText(new Gson().toJson(result));
+                            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                            textView.setText(gson.toJson(result));
                         } else {
                             Log.e("MainActivity", "Response received but request not successful. Response: " + response.raw());
+                            textView.setText("Response received but request not successful. Response: " + response.raw());
                         }
                     }
 
                     @Override
                     public void onFailure(Throwable t) {
-                        Log.e("MainActivity", "Request error!!!");
+                        Log.e("MainActivity", "Request error!");
                     }
                 });
             }
